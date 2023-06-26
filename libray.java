16d2f31c963f49aa7118e438d003e4850f6bc546
JH7871 <112674898+JH7871@users.noreply.github.com>
2023-06-26 09:08:31 +0530

    Add files via upload

---
+++ libray.java
@@ -0,0 +1,81 @@
+
+
+/**
+ * Class libray
+ */
+public class libray {
+
+  //
+  // Fields
+  //
+
+  public char student_nanme_;
+  public int student_id;
+  
+  //
+  // Constructors
+  //
+  public libray () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of student_nanme_
+   * @param newVar the new value of student_nanme_
+   */
+  public void setStudent_nanme_ (char newVar) {
+    student_nanme_ = newVar;
+  }
+
+  /**
+   * Get the value of student_nanme_
+   * @return the value of student_nanme_
+   */
+  public char getStudent_nanme_ () {
+    return student_nanme_;
+  }
+
+  /**
+   * Set the value of student_id
+   * @param newVar the new value of student_id
+   */
+  public void setStudent_id (int newVar) {
+    student_id = newVar;
+  }
+
+  /**
+   * Get the value of student_id
+   * @return the value of student_id
+   */
+  public int getStudent_id () {
+    return student_id;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       char
+   */
+  public char book_name()
+  {
+  }
+
+
+  /**
+   * @return       int
+   */
+  public int serial_no_()
+  {
+  }
+
+
+}
---
+++ student.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class student
+ */
+public class student {
+
+  //
+  // Fields
+  //
+
+  public int student_id;
+  public char student_name;
+  private int student_phone_no_;
+  
+  //
+  // Constructors
+  //
+  public student () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of student_id
+   * @param newVar the new value of student_id
+   */
+  public void setStudent_id (int newVar) {
+    student_id = newVar;
+  }
+
+  /**
+   * Get the value of student_id
+   * @return the value of student_id
+   */
+  public int getStudent_id () {
+    return student_id;
+  }
+
+  /**
+   * Set the value of student_name
+   * @param newVar the new value of student_name
+   */
+  public void setStudent_name (char newVar) {
+    student_name = newVar;
+  }
+
+  /**
+   * Get the value of student_name
+   * @return the value of student_name
+   */
+  public char getStudent_name () {
+    return student_name;
+  }
+
+  /**
+   * Set the value of student_phone_no_
+   * @param newVar the new value of student_phone_no_
+   */
+  private void setStudent_phone_no_ (int newVar) {
+    student_phone_no_ = newVar;
+  }
+
+  /**
+   * Get the value of student_phone_no_
+   * @return the value of student_phone_no_
+   */
+  private int getStudent_phone_no_ () {
+    return student_phone_no_;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void get_loan_book()
+  {
+  }
+
+
+  /**
+   */
+  public void new_operation()
+  {
+  }
+
+
+}
